4df18969cc287a53004621ebc41802a4cf5e7581
pbriant <psbriant@q.com>
2017-03-14 18:50:11 -0700

    Added new database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,15 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# up top
+import dj_database_url
+# lower - replace existing
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
