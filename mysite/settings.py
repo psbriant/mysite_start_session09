214aeec50c11e8b89475345ccb89311ce8ee6df9
Ubuntu <ubuntu@ip-172-31-31-181.us-west-2.compute.internal>
2017-03-15 03:45:04 +0000

    Created new files for layers 2 and 4.

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'x=&k)d5b1qikw)r-b06t9p0t#o8=vsv^0iaomo2u3q^lm+jemt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-54-214-125-70.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
